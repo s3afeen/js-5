5431c89e87c24ca6f3324e2019fee6ba7d4c26c9
s3afeen <zaidsafeen30@gmail.com>
2024-06-23 00:48:33 +0300

    js-5

---
+++ task5.js
@@ -0,0 +1,89 @@
+//--------------------1
+// let button = document.getElementById('clickk');
+
+//         button.addEventListener('click', function()
+//          {
+//             alert('welcom');
+//         });
+
+
+
+//--------------------2
+// var mybutt = document.getElementById('mybutt');
+// var paragraph = document.getElementById('paragraph');
+
+// mybutt.addEventListener('click', function()
+// {
+//     paragraph.textContent = "Saddam Hussein";
+// });
+
+
+
+//--------------------3
+// const name = document.getElementById('name');
+// name = document.addEventListener('dblclick', function() 
+// { 
+//     name.style.backgroundColor = "red";
+// });
+
+
+
+//--------------------4
+// var x =  document.getElementById('clickk');
+// x = document.addEventListener('mouseenter', function()
+// {
+//     clickk.textContent = "sss";
+// });
+
+
+
+//--------------------5
+// const x = document.getElementById('cilckk');
+// x = document.addEventListener("mouseleave", function()
+// {
+//    clickk.textContent = "aaaa";
+// });
+
+
+
+//--------------------6
+// document.getElementById('all');
+// all.addEventListener('submit', function() {
+//     document.write('Form not submitted');
+// });
+
+
+
+//--------------------7
+// var text = document.getElementById('text');
+// text = document.addEventListener("fucus", function()
+// {
+//   text.classList.add("highlight");
+// });
+
+
+
+//--------------------8
+// var  text = document.getElementById('text');
+// text = document.addEventListener("blur", function()
+// {
+//     text.classList.remove("highlight");
+// });
+
+
+
+//--------------------9
+// const input = document.getElementById('input');
+// input.addEventListener("keydown", function(event)
+// {
+//     console.log(`kay pressed : ${event.key}`);
+// });
+
+
+
+//--------------------10
+// var x = document.getElementById('mydiv');
+// document.addEventListener('click', function()
+// {
+//     mydiv.classList.toggle('highlight');
+// });
